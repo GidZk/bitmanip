e1b9c051bfc7333a12152a6c8a75b4ad71aa402d
John Lindström Gidskehaug <johnlindstrom92@hotmail.com>
2019-03-17 10:49:04 +0100

    added planetSys

--- orbitSystem/orbitSys.cpp
+++ orbitSystem/orbitSys.cpp
@@ -4,61 +4,60 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
-#include<vector>
+#include <vector>
+#include <sstream>
 using namespace std;
 #define EARTH_DAYS 365
 #define MARS_DAYS 687
 
-int main (){
-int earth, mars;
-int nCase = 1;
-int nOfDays;
-string tmp;
-vector<int> output;
+void printAll(vector<int> list);
 
-    while (cin >> earth && cin >> mars){
+int main()
+{
+    int earth, mars, nOfDays, testCase;
 
-        // TODO : how many earth days has passed ? earth is now 0
-        mars = abs(mars - earth);
-        earth = earth - earth;
+    vector<int> output;
 
-        while (1){
+    while (cin >> earth && cin >> mars)
+    {
+        nOfDays = 0;
+        nOfDays = EARTH_DAYS - earth;
+
+        if (earth == 0 && mars == 0)
+        {
+            output.push_back(0);
+        }
+        else
+        {
             
-            mars = mars + EARTH_DAYS;
-            mars = mars % MARS_DAYS; 
-            if (mars == 0){
-           
-                output.push_back(nOfDays);
-   
-                
+            mars = mars + nOfDays;
+            mars %= MARS_DAYS;
+
+            while (1)
+            {
+                if (mars == 0)
+                {
+                    output.push_back(nOfDays);
+                    break;
+                }
+                mars += EARTH_DAYS;
+                mars %= MARS_DAYS;
+                nOfDays += EARTH_DAYS;
             }
-
-
         }
-                
-
-
-            
-        }
-    
-            cout << "finish \n";
-        //cout << input + "\n";
-
-   
-
-
-
-
-    
-
-}
-
-
-void printAll(vector<string> list){
-     for (int i = 0; i < list.size(); i++ ){
-        cout << list[i] << "\n";
     }
 
+    printAll(output);
+}
 
- 
+void printAll(vector<int> list)
+{
+    std::stringstream output;
+
+    for (int i = 1; i <= list.size(); i++)
+    {
+        output << "Case " << i << ": " << list.at(i-1) << endl;
+       
+    }
+     cout << output.str();
 }
